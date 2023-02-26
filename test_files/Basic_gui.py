4472d0b8ae2a3d41029660d374f5a0a1e2fefd41
Kartik9250 <kartikrana9250@gmail.com>
2023-02-26 14:37:46 +0530

    Added classes and text updation after a button event

--- test_files/Basic_gui.py
+++ test_files/Basic_gui.py
@@ -2,25 +2,32 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-def clicked():
-    print("clicked")
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(810,540, 300,300)
+        self.setWindowTitle("learning qt")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("first test app")
+        self.label.move(50,50)
+        
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you clicked the button")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
 
 def Main_window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-
-    win.setGeometry(540,810, 300,300)
-    win.setWindowTitle("learning qt")
-    
-    label = QtWidgets.QLabel(win)
-    label.setText("first test app")
-    label.move(50,50)
-    
-    b1 = QtWidgets.QPushButton(win)
-    b1.setText("click me")
-
-    b1.clicked.connect(clicked)
-
+    win = MyWindow()
     win.show()
     sys.exit(app.exec_())
 
